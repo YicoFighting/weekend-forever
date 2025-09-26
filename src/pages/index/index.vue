522b5458201683e9f1279fd655968b3532b5929a
liu <zhliu@sziroad.com>
2025-09-26 18:02:48 +0800

    feat(index): 测试 format

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,19 @@
 <template>
   <view>
     <text class="bg-blue-500">首页</text>
-    <scroll-view scroll-x>
-      <view v-for="item in 10" :key="item">
-        <text>{{ item }}</text>
-      </view>
-    </scroll-view>
     <wd-button type="primary">点击我</wd-button>
   </view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+  const test = () => {
+
+
+
+
+    
+  }
+</script>
 
 <style scoped lang="scss"></style>
