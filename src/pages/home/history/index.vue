def4c521136c9af1e00a8a385cc9d9c7912b8185
YicoFighting <2604482363@qq.com>
2025-10-22 18:05:07 +0800

    feat(history): 添加历史记录页面的搜索和筛选功能

--- src/pages/home/history/index.vue
+++ src/pages/home/history/index.vue
@@ -1,48 +1,132 @@
 <template>
-  <view class="history p-[10px]">
-    <wd-steps vertical dot :active="10">
-      <wd-step v-for="value in 5" :key="value">
-        <template #title> 2025-09-29 {{ value }}:00:00 </template>
-        <template #description>
-          <!-- 先添加换班历史，然后再添加值班 -->
-          <ul class="flex flex-col gap-2">
-            <li
-              class="flex flex-col border border-solid border-gray-300 rounded-md p-2 cursor-pointer"
-              @tap="handleVisible"
-            >
-              <h4 class="text-base">标题</h4>
-              <text class="text-sm">内容</text>
-            </li>
-          </ul>
-        </template>
-      </wd-step>
-    </wd-steps>
+  <xl-navbar title="历史记录" show-back :navBarStyle="{ backgroundColor: 'rgb(248, 248, 248)' }">
+    <view class="history w-full overflow-hidden">
+      <view class="search">
+        <wd-search
+          v-model="filter.keyword"
+          hide-cancel
+          placeholder-left
+          placeholder="搜索记录内容、人员姓名等"
+        >
+          <template #prefix>
+            <wd-icon class="pl-[16px]" name="search1" size="18px" color="#999999"></wd-icon>
+          </template>
+        </wd-search>
+      </view>
 
-    <xl-item-card v-model="visible"></xl-item-card>
-  </view>
+      <view class="filter bg-[#fff]">
+        <view class="date gap-[12px] flex items-center pl-[15px] mb-[16px]">
+          <wd-datetime-picker v-model="filter.startTime" type="date">
+            <view
+              class="w-[190rpx] px-[12px] py-[5px] bg-[#f8f9fa] text-[28rpx] color-[#bfbfbf] text-center cursor-pointer flex gap-[8px] items-center"
+            >
+              <text>2025-10-22</text>
+              <wd-icon name="calendar" size="16px"></wd-icon>
+            </view>
+          </wd-datetime-picker>
+          <text>至</text>
+          <wd-datetime-picker v-model="filter.endTime" type="date">
+            <view
+              class="w-[190rpx] px-[12px] py-[5px] bg-[#f8f9fa] text-[28rpx] color-[#bfbfbf] text-center cursor-pointer flex gap-[8px] items-center"
+            >
+              <text>2025-10-22</text>
+              <wd-icon name="calendar" size="16px"></wd-icon>
+            </view>
+          </wd-datetime-picker>
+        </view>
+        <view class="types gap-[8px] flex items-center pl-[15px]">
+          <view
+            class="type"
+            v-for="item in typeList"
+            :key="item.value"
+            :class="{ active: filter.type === item.value }"
+            @tap="selectType(item.value)"
+          >
+            <text>{{ item.label }}</text>
+          </view>
+        </view>
+      </view>
+    </view>
+  </xl-navbar>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+import dayjs from 'dayjs';
 import { ref } from 'vue';
 
-const visible = ref(false);
+type FilterType = 'all' | 'duty' | 'change' | 'other';
 
-const handleVisible = () => {
-  visible.value = true;
+const typeList: { label: string; value: FilterType }[] = [
+  {
+    label: '全部',
+    value: 'all',
+  },
+  {
+    label: '值班安排',
+    value: 'duty',
+  },
+  {
+    label: '换班记录',
+    value: 'change',
+  },
+  {
+    label: '事项记录',
+    value: 'other',
+  },
+];
+
+const filter = ref({
+  keyword: '',
+  startTime: dayjs().startOf('month').valueOf(),
+  endTime: dayjs().endOf('month').valueOf(),
+  type: 'all' as FilterType,
+});
+
+const selectType = (value: FilterType) => {
+  filter.value.type = value;
 };
 </script>
 
 <style lang="scss" scoped>
 .history {
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.05);
-}
-.item-left,
-.item-right {
-  flex: 1 0 0;
-  text-align: left;
-  background-color: #fff;
-  height: 200rpx;
-  border-radius: 10rpx;
+  box-sizing: border-box;
+  height: calc(100% - 55px);
+  background: rgb(248, 248, 248);
+  display: flex;
+  flex-direction: column;
+  .search {
+    // padding-bottom: 8px;
+    :deep(.wd-search__field) {
+      .wd-icon-search {
+        display: none;
+      }
+      .wd-search__input {
+        padding: 0;
+      }
+    }
+  }
+  .filter {
+    border-top: 1px solid #f0f0f0;
+    border-bottom: 1px solid #f0f0f0;
+    padding: 16px 20px;
+  }
+  .types {
+    .type {
+      padding: 8px 12px;
+      color: #666666;
+      background-color: #f8f9fa;
+      font-size: 13px;
+      border-radius: 10px;
+      border: 1px solid transparent;
+      transition: all 0.3s ease;
+      cursor: pointer;
+    }
+    .active {
+      background-color: #e3f2fd;
+      border-color: #007aff;
+      color: #007aff;
+      font-weight: 500;
+    }
+  }
 }
 </style>
